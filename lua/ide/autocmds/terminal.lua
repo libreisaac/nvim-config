35ce72c90b1f7cd3f1de52ca1242ae9464b2bae3
Isaac <165521416+libreisaac@users.noreply.github.com>
2024-05-05 23:17:14 +0100

    Hide normal and relative numbers in terminals

--- lua/ide/autocmds/terminal.lua
+++ lua/ide/autocmds/terminal.lua
@@ -2,6 +2,6 @@ vim.api.nvim_create_autocmd(
     {"TermOpen"}, {
     callback = function(ev)       
         vim.api.nvim_command("startinsert")
-        vim.api.nvim_command("setlocal nonumber")
+        vim.api.nvim_command("setlocal norelativenumber nonumber")
     end,
 })
